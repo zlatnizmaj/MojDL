df98cf479b7a83c735e0362621bcf885718e6860
NamNg <biznis68@yahoo.com>
2020-10-19 22:06:01 +0700

    commited DL

--- autoEncoderTF.py
+++ autoEncoderTF.py
@@ -3,3 +3,24 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+__author__ = "Nam Ng"
+
+from functools import  partial
+import  tensorflow as tf
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, intermediate_dim):
+        super(Encoder, self).__init__()
+        self.hidden_layer = tf.keras.layers.Dense(
+            units=intermediate_dim,
+            activation=tf.nn.relu,
+            kernel_initializer='he_uniform'
+        )
+        self.output_layer = tf.keras.layers.Dense(
+            units=intermediate_dim,
+            activation=tf.nn.sigmoid
+        )
+
+    def call(self, input_features):
+        activation = self.hidden_layer(input_features)
+        return self.output_layer(activation)
