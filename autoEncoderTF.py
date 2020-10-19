57d8b46ca1be49c5585b1951a8b708f301e23e50
NamNg <biznis68@yahoo.com>
2020-10-19 21:55:41 +0700

    added 3 line

---
+++ autoEncoderTF.py
@@ -0,0 +1,5 @@
+"""Implementation of vanila autoencoder in TensorFlow 2.0 Subclassing API"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
